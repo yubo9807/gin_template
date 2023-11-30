cedf6b4d1dfba1e763e41d93c32c343a355b9fab
yubo9807 <yubo9807@gmail.com>
2023-11-30 14:06:35 +0800

    chore: sql connect timeout

--- configs/config.go
+++ configs/config.go
@@ -7,9 +7,8 @@ import (
 )
 
 type ConfigType struct {
-	Port    int
-	Prefix  string
-	TimeOut int `yaml:"timeOut"`
+	Port   int
+	Prefix string
 
 	TokenValidTime         int64 `yaml:"tokenValidTime"`
 	TokenExceedRefreshTime int64 `yaml:"tokenExceedRefreshTime"`
@@ -20,7 +19,6 @@ var Config ConfigType
 const template = `
 prefix: "/baseUrl"  # 路由前缀
 port: 8080  # 启动端口
-timeOut: 5  # 请求超时时间(s)
 
 tokenValidTime: 7200  # 令牌有效时间(s)
 TokenExceedRefreshTime: 86400  # 令牌超过刷新时间(s)
